4b678bc7bc8cc6ea8e0d1a1790abfca39a8bbe28
honhon012 <honhon015@hotmail.com>
2017-10-29 22:03:27 +0700

    Create router to 4 Complex Form

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import MainPage from '@/components/MainPage'
+// import UpdateHero from '@/components/UpdateHero'
+// import UpdateItem from '@/components/UpdateItem'
+import UpdatePlayer from '@/components/UpdatePlayer'
+// import UpdateTeam from '@/components/UpdateTeam'
 
 Vue.use(Router)
 
@@ -10,6 +14,26 @@ export default new Router({
       path: '/',
       name: 'MainPage',
       component: MainPage
+    },
+    // {
+    //   path: '/updatehero',
+    //   name: 'UpdateHero',
+    //   component: UpdateHero
+    // },
+    // {
+    //   path: '/updateitem',
+    //   name: 'UpdateItem',
+    //   component: UpdateItem
+    // },
+    {
+      path: '/updateplayer',
+      name: 'UpdatePlayer',
+      component: UpdatePlayer
+    // },
+    // {
+    //   path: '/updateteam',
+    //   name: 'UpdateTeam',
+    //   component: UpdateTeam
     }
   ]
 })
