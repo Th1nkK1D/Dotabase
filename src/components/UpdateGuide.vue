73a238a594d9fe607a85768a3494a30aed140d9d
iamgide <gittipat.1997@hotmail.com>
2017-10-29 23:32:03 +0700

    Updateguide

--- src/components/UpdateGuide.vue
+++ src/components/UpdateGuide.vue
@@ -3,6 +3,93 @@
     <div>
         <h1>Update Guide</h1>
         <br>
+          <label for="name_guide">Guide Name</label>
+			    <input type="text" name="name_guide" >
+			    <br>
+
+          <label for="hero_Select">Select Hero</label>
+			    <input type="text" name="hero_Select" >
+			    <br>
+
+          Purchase Order
+          <br>
+
+          <label for="po_name">Po Name</label>            
+			    <input type="text" name="po_name" >
+			    <br>
+
+<!-- ช่องitemที่จะใส่ -->
+          <div>
+
+          </div>
+          <label for="item_name">Item Name</label>
+			    <input type="text" name="item_name" >
+			    <br>
+
+<!-- ช่อง items ทั้งหมด -->
+          <div>
+
+          </div>
+
+          Skill Sequence
+          <br>
+
+<!-- first skill -->
+          <div>
+            <div>
+               <img src="https://dota2.gamepedia.com/media/dota2.gamepedia.com/c/c1/Mana_Break_icon.png?version=c19f51e00b35bbd4c06190af131f7a00">
+                First Skill
+            </div>
+            <br>
+            <div>
+              <!-- up skill 1-25 -->
+            </div>
+          </div>
+
+<!-- second skill -->
+          <div>
+            <img src="https://dota2.gamepedia.com/media/dota2.gamepedia.com/c/ce/Blink_%28Anti-Mage%29_icon.png?version=f5de5ca6a6688d1b36f78f3af80cfe92">
+            Second Skill
+          </div>
+          <br>
+          <div>
+              <!-- up skill 1-25 -->
+          </div>
+
+<!-- Third skill -->
+          <div>
+            <img src="https://dota2.gamepedia.com/media/dota2.gamepedia.com/a/a6/Spell_Shield_icon.png?version=5238b95c34062f0ecbd6d663056597a8">
+            Third Skill
+          </div>
+          <br>
+          <div>
+              <!-- up skill 1-25 -->
+          </div>
+
+<!-- Fourth skill -->
+          <div>
+            <img src="https://dota2.gamepedia.com/media/dota2.gamepedia.com/f/fe/Mana_Void_icon.png?version=35c88d6b4098081cb4895350f184c6f0">
+            Fourth Skill
+          </div>
+          <br>
+          <div>
+              <!-- up skill 1-25 -->
+          </div>
+
+<!-- Talent Level -->
+          <div>
+            <img src="https://lh3.googleusercontent.com/qShpEIkHJJ36Jp7NPwyuGSs6KpzwthEoOJhHVWGIrv6pNtFySj2DhDsG479TWYmftE4=w300">
+            Talent Level
+          </div>
+          <br>
+          <div>
+              <!-- up skill 1-25 -->
+          </div>
+
+<!-- Talent Tree -->
+          <div>
+            <img src="https://i1.wp.com/thegamehaus.com/wp-content/uploads/2017/01/morphling.png">
+          </div>
 
         <br>
         <button>Submit</button>
