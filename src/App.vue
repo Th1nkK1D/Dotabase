b81970a5538cace1b795c5d39b04ced0f04632e3
Withee Poositasai <witheep@gmail.com>
2017-10-29 19:03:25 +0700

    Clean up component, Init Firabase

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,29 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
 
 <script>
+import Firebase from 'firebase'
+
+// Config Firebase
+var config = {
+  apiKey: "AIzaSyAxiBf44qWEyekWQcxuDWzhEuzXsSwvt3s",
+  authDomain: "dotabase-fe206.firebaseapp.com",
+  databaseURL: "https://dotabase-fe206.firebaseio.com",
+  projectId: "dotabase-fe206",
+  storageBucket: "dotabase-fe206.appspot.com",
+  messagingSenderId: "737827328503"
+};
+
+Firebase.initializeApp(config)
+
 export default {
   name: 'app'
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/MainPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    Welcome to Dotabase!
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainPage',
+  data () {
+    return {
+      // Add data here
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
