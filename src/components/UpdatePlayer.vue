c5df8a4fb0d72dbdaeb72d15c0c159ae6a3d9211
honhon012 <honhon015@hotmail.com>
2017-11-04 17:21:05 +0700

    connect firebase(player team)

--- src/components/UpdatePlayer.vue
+++ src/components/UpdatePlayer.vue
@@ -4,36 +4,38 @@
         <h1>Update Player</h1>
         <br>
         <label for="name">Player Name</label>
-        <input type="text" name="name">
+        <input type="text" name="name" v-model="Player.name">
         <br>
         <label for="playeravatar">Avatar</label>
-        <input type="text" name="playeravatar">
+        <input type="text" name="playeravatar" v-model="Player.avatar">
         <br>
         <label for="teamname">Team Name</label>
-        <input type="text" name="teamname">
-        <br>
-        <label for="region">Region</label>
-        <input type="text" name="region">
+        <input type="text" name="teamname" v-model="Player.teamName">
         <br>
         <label for="playerrole">Role</label>
-        <input type="text" name="playerrole">
+        <input type="text" name="playerrole" v-model="Player.role">
         <br>
         <label for="solommr">MMR Solo</label>
-        <input type="number" name="solommr">
+        <input type="number" name="solommr" v-model="Player.mmrSolo">
         <br>
         <label for="partymmr">MMR Party</label>
-        <input type="number" name="partymmr">
+        <input type="number" name="partymmr" v-model="Player.mmrParty">
+        <br>
+        <label for="matchplayed">Match Played</label>
+        <input type="number" name="matchplayed" v-model="Player.matchPlayed">
         <br>
         <br>
         <br>
         <h2>Hero Rank:</h2>
         <label for="heroname">Hero Name</label>
-        <input type="number" name="heroname">
+        <input type="text" name="heroname" v-model="Player.HeroRank.heroName">
         <br>
         <label for="heroscore">Hero Score</label>
-        <input type="number" name="heroscore">
+        <input type="number" name="heroscore" v-model="Player.HeroRank.score">
         <br>
-        <button>Submit</button>
+        <button v-on:click="submit()">Submit</button>
+        <br>
+        {{Player}}
     </div>
 
 
@@ -41,11 +43,30 @@
 </template>
 
 <script>
+import Firebase from 'firebase'
+var playerdb = Firebase.database().ref('/Players')
 export default {
   name: 'UpdatePlayer',
   data () {
     return {
-      // Add data here
+      Player: {
+        name: '',
+        avatar: '',
+        teamName: '',
+        role: '',
+        mmrSolo: 0,
+        mmrParty: 0,
+        matchPlayed: 0,
+        HeroRank: {
+          heroName: '',
+          score: 0,
+        }
+      }
+    }
+  },
+  methods: {
+    submit: function() {
+      playerdb.child(this.Player.name).set(this.Player)
     }
   }
 }
