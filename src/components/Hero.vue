4667afe532fcd630ffefe6869df797b23db82521
Withee Poositasai <witheep@gmail.com>
2017-11-09 19:09:43 +0700

    Basic readable hero page, minor bolt fixed

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,7 +1,33 @@
 <template>
   <div>
     <h1>{{hero.name}}</h1>
-    {{hero}}
+    <img :src="hero.avatar" :alt="hero.name">
+    <p>Attribute: {{hero.attribute}}</p>
+    <p>Role: <span v-for="role in hero.roles" :key="role">{{role}} </span></p>
+    <p>{{hero.lore}}</p>
+
+    <h2>Stat</h2>
+    <p>Strength: {{hero.strBase}} +{{hero.strGain}}</p>
+    <p>Agility: {{hero.agiBase}} +{{hero.agiGain}}</p>
+    <p>Intelligence: {{hero.intBase}} +{{hero.intGain}}</p>
+    <p>Health: {{hero.hp}} (+{{hero.hpRegen}}/sec)</p>
+    <p>Mana: {{hero.mana}} (+{{hero.manaRegen}}/sec)</p>
+    <p>Damage: {{hero.damageMin}}-{{hero.damageMax}}</p>
+    <p>Armor: {{hero.armor}}</p>
+    <p>Magic Resistance: {{hero.magicResistance}}</p>
+    <p>Move Speed: {{hero.moveSpeed}}</p>
+    <p>Attack Range: {{hero.attackRange}}</p>
+    <p>Attack Speed: {{hero.attackSpeed}}</p>
+
+    <h2>Skills</h2>
+    <div v-for="skill in hero.skills" :key="skill.slot">
+      <h4>{{skill.name}}</h4>
+      <img :src="skill.icon" :alt="skill.name">
+      <p>{{skill.description}}</p>
+      <ul>
+        <li v-for="attr in skill.attributes" :key="attr.name">{{attr.name}}: <i>{{attr.value}}</i></li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -9,8 +35,6 @@
 import Firebase from 'firebase'
 
 var heroDB = Firebase.database().ref('/Heroes')
-
-console.log(this.heroKey)
 
 export default {
   name: 'Hero',
