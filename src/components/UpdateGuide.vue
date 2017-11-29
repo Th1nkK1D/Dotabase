0009857947e7349a2aa664d4701e0c11cd171d59
Withee Poositasai <witheep@gmail.com>
2017-11-29 14:18:23 +0700

    Add talent tree select

--- src/components/UpdateGuide.vue
+++ src/components/UpdateGuide.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <h1 class="title is-1">Create Guide</h1>
     <b-field>
       <b-input type="text" placeholder="Guidename" v-model="guide.name"></b-input>
     </b-field>
@@ -8,43 +9,67 @@
       <b-autocomplete v-model="heroSearch" placeholder="Search hero..." :data="filterdHeroList" field="name" :keep-first="true" @select="option => guide.hero = option ? option : null"></b-autocomplete>
     </b-field>
 
-    <button class="button" @click="addPC()">+ PurchaseCategory</button>
+    <div v-if="guide.hero">
 
-    <!-- PurchaseCategory List -->
-    <div class="columns is-multiline">
-      <div class="column" v-for="(cat,c) in guide.purchaseCategory" :key="c">
-        <div class="box">
-          <b-field>
-            <b-input type="text" placeholder="Category Name" size="is-small" v-model="cat.name"></b-input>
-          </b-field>
-          <div class="columns is-multiline">
-            <div class="column">
-              <img :src="item.icon" v-for="(item,i) in cat.items" :key="i" @click="removeItem(c,i)">
+      <h1 class="title">Purchase Category</h1>
+      <!-- PurchaseCategory -->
+      <div>
+        <button class="button" @click="addPC()">+ PurchaseCategory</button>
+
+        <div class="columns is-multiline">
+          <div class="column" v-for="(cat,c) in guide.purchaseCategory" :key="c">
+            <div class="box">
+              <b-field>
+                <b-input type="text" placeholder="Category Name" size="is-small" v-model="cat.name"></b-input>
+              </b-field>
+              <div class="columns is-multiline">
+                <div class="column">
+                  <img :src="item.icon" v-for="(item,i) in cat.items" :key="i" @click="removeItem(c,i)">
+                </div>
+              </div>
+              <b-field>
+                <b-autocomplete v-model="itemSearch" placeholder="Search item..." :data="filterdItemList" field="name" size="is-small" :keep-first="true" @select="option => option ? cat.items.push(option) : null"></b-autocomplete>
+              </b-field>
             </div>
           </div>
-          <b-field>
-            <b-autocomplete v-model="itemSearch" placeholder="Search item..." :data="filterdItemList" field="name" size="is-small" :keep-first="true" @select="option => option ? cat.items.push(option) : null"></b-autocomplete>
-          </b-field>
         </div>
-      </div>
-    </div><!-- End of PurchaseCategory List -->
+      </div><!-- End of PurchaseCategory -->
 
-    <!-- Learn Order -->
-    <div class="columns is-multiline" v-if="guide.hero">
-      <!-- Skills -->
-      <div class="column is-12" v-for="(skill,s) in guide.hero.skills" :key="s">
-        <img :src="skill.icon" alt="">
-        <b-radio v-for="(learn,l) in guide.learnOrder" :key="l" :native-value="{isSkill: true, slot: s}" v-model="guide.learnOrder[l]" size="is-small"></b-radio>
-      </div><!-- End of Skills -->
+      <h1 class="title">Learn Order</h1>
+      <!-- Learn Order -->
+      <div class="columns is-multiline">
+        <!-- Skills -->
+        <div class="column is-12" v-for="(skill,s) in guide.hero.skills" :key="s">
+          <img :src="skill.icon" alt="">
+          <b-radio v-for="(learn,l) in guide.learnOrder" :key="l" :native-value="{isSkill: true, slot: s}" v-model="guide.learnOrder[l]" size="is-small"></b-radio>
+        </div><!-- End of Skills -->
 
-      <!-- Talents -->
-      <div class="column is-12">
-        Talent
-        <b-radio v-for="(learn,l) in guide.learnOrder" :key="l" :native-value="{isSkill: false}" v-model="guide.learnOrder[l]" size="is-small"></b-radio>
-      </div><!-- End of Talents -->
-    </div><!-- End of Learn Order -->
+        <!-- Talents -->
+        <div class="column is-12">
+          Talent
+          <b-radio v-for="(learn,l) in guide.learnOrder" :key="l" :native-value="{isSkill: false}" v-model="guide.learnOrder[l]" size="is-small"></b-radio>
+        </div><!-- End of Talents -->
+      </div><!-- End of Learn Order -->
 
-    {{guide.learnOrder}}
+      <h1 class="title">Talent Tree</h1>
+      <!-- Talent Tree -->
+      <div>
+        <div class="columns" v-for="(talentLvl,tl) in guide.talentTree" :key="tl">
+          <div class="column">
+            <b-radio v-model="guide.talentTree[3-tl]" :native-value="0"></b-radio>
+            {{guide.hero.talents[3-tl][0]}}
+          </div>
+          <div class="column is-narrow">{{25 - tl*5}}</div>
+          <div class="column">
+            <b-radio v-model="guide.talentTree[3-tl]" :native-value="1"></b-radio>
+            {{guide.hero.talents[3-tl][1]}}
+          </div>
+        </div>
+        {{guide.talentTree}}
+      </div><!-- End of Talent Tree -->
+    
+    </div>
+
   </div>
 
 </template>
@@ -63,6 +88,7 @@ export default {
        name: '',
        hero: '',
        learnOrder: [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null],
+       talentTree: [null,null,null,null],
        purchaseCategory: []
      },
      heroSearch: '',
@@ -85,7 +111,7 @@ export default {
       return this.items.filter((item) => {
         return item.name.toLowerCase().indexOf(this.itemSearch.toLowerCase()) >= 0
       })
-    }
+    },
   },
 
   methods: {
