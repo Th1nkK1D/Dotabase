fe8eb039f84dcbf5930502c86b61482c1d3945a9
honhon012 <honhon015@hotmail.com>
2017-12-07 18:08:11 +0700

    Nav

--- src/components/ItemsList.vue
+++ src/components/ItemsList.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Items</h1>
+    <h1 class="title is-1">Items</h1>
     <div class="columns">
       <div class="column">
         <ul>
--- src/components/PlayersList.vue
+++ src/components/PlayersList.vue
@@ -1,14 +1,6 @@
 <template>
   <div>
-    <h1>Players</h1>
-
-    
-    <br>
-    <br>
-    
-
-
-
+    <h1 class="title is-1">Players</h1>
     <div class="columns">
       <div class="column">
         <ul>
