66858807ec1c213bb6bad2b03a0dca2553588e08
honhon012 <honhon015@hotmail.com>
2017-12-08 16:40:42 +0700

    change into table

--- src/components/TeamsList.vue
+++ src/components/TeamsList.vue
@@ -11,41 +11,34 @@
       </div>
     </div>
 
-    <div class="columns">
-      <div class="column">
-        <ul>
-          <li v-for="(team,key) in teams" v-bind:key="key">
+    <h3 class="title is-3">Highest Average MMR Teams</h3>
+    <table class="table is-fullwidth is-striped">
+      <thead>
+        <tr>
+          <th>Rank</th>
+          <th>Team Name</th>
+          <th>Region</th>
+          <th>Average MMR</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(team,key) in teams" v-bind:key="key">
+          <td>
+            {{key+1}}
+          </td>
+          <td>
             <router-link v-bind:to="'team/'+team['.key']">{{team.name}}</router-link>
-          </li>
-        </ul>
-      </div>
-      <div class="column">
-        <h3 class="title is-3">Highest Average MMR Teams</h3>
-        <table class="table">
-          <thead>
-            <tr>
-              <th>Rank</th>
-              <th>Team Name</th>
-              <th>Average MMR</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="(team,key) in teams" v-bind:key="key">
-              <td>
-                {{key+1}}
-              </td>
-              <td>
-                <router-link v-bind:to="'team/'+team['.key']">{{team.name}}</router-link>
-              </td>
-              <td>
-                num
-              </td>
-            </tr>
-          </tbody>
-        </table>
-        <br>
-      </div>
-    </div>
+          </td>
+          <td>
+            {{team.region}}
+          </td>
+          <td>
+            num
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <br>
 
   </div>
 </template>
