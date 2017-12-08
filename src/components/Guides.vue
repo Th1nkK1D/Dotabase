5909b5ab6c3cffca2034c61a508bcb46fe33a0a2
iamgide555 <gittipat.1997@hotmail.com>
2017-12-08 13:56:15 +0700

    guidefinalla

--- src/components/Guides.vue
+++ src/components/Guides.vue
@@ -1,65 +1,123 @@
 <template>
   <div>
-    <h1>Guide</h1> <br>
-    <p>{{guide.name}}</p>
+    <h1 class="title is-1">{{guide.name}}</h1> <br>
+    <h5 class="title is-5">
+      <p>Hero : {{guide.hero}}</p>
+    </h5>
     <img :src="hero.avatar" :alt="hero.name">
-    <p>{{guide.hero}}</p><br>
-    <div v-for="purchase in guide.purchaseCategory" :key="purchase.slot">
-      {{purchase.name}}
-      <div v-for="item in purchase.items" :key="item">
-        <img :src="items[item].icon" :alt="item"> {{item}}
+    <div class="columns">
+      <div v-for="purchase in guide.purchaseCategory" :key="purchase.slot" class="column">
+        <h3 class="title is-3">{{purchase.name}}</h3>
+
+        <div v-for="item in purchase.items" :key="item">
+          <img :src="items[item].icon" :alt="item"> {{item}}
+        </div>
+        <br>
       </div>
+      =======
+      <h1>Guide</h1> <br>
+      <p>{{guide.name}}</p>
+      <img :src="hero.avatar" :alt="hero.name">
+      <p>{{guide.hero}}</p><br>
+      <div v-for="purchase in guide.purchaseCategory" :key="purchase.slot">
+        {{purchase.name}}
+        <div v-for="item in purchase.items" :key="item">
+          <img :src="items[item].icon" :alt="item"> {{item}}
+        </div>
+        <br>
+      </div>
+
+      <table>
+        <tr v-for="(skill,Sindex) in hero.skills" :key="skill.name">
+          <div>{{skill.name}}</div><br>
+          <td v-for="(learn,Lindex) in guide.learnOrder" :key="Lindex" v-if="learn != undefined">
+            <div v-if="learn != 0 && Sindex == learn.slot">
+              1
+            </div>
+            <div v-if="learn != 0 && Sindex != learn.slot">
+              0
+            </div>
+            <div v-if="learn == 0">
+              0
+            </div>
+          </td>
+        </tr>
+        <tr>
+          talents
+          <td v-for="(learn,Lindex) in guide.learnOrder" :key="Lindex" v-if="learn != undefined">
+            <div v-if="learn != 0 && learn.isSkill">
+              0
+            </div>
+            <div v-if="learn != 0 && !learn.isSkill">
+              1
+            </div>
+            <div v-if="learn == 0">
+              0
+            </div>
+          </td>
+        </tr>
+      </table>
+
+      <table>
+        <tr v-for="(skill,Sindex) in hero.skills" :key="skill.name">
+          <h4 class="title is-4">
+            <div>{{skill.name}}</div>
+          </h4>
+          <div class="columns">
+            <td v-for="(learn,Lindex) in guide.learnOrder" :key="Lindex" v-if="learn != undefined" class="column">
+              <div class="yes" v-if="learn != 0 && Sindex == learn.slot">
+                <center>
+                  {{Lindex+1}}
+                </center>
+              </div>
+              <div class="nope" v-if="learn != 0 && Sindex != learn.slot">
+                {{Lindex+1}}
+              </div>
+              <div class="nope" v-if="learn == 0">
+                {{Lindex+1}}
+              </div>
+            </td>
+          </div>
+        </tr>
+        <tr>
+          <h4 class="title is-4">
+            <div>TarentTree</div>
+          </h4>
+          <div class="columns">
+            <td v-for="(learn,Lindex) in guide.learnOrder" :key="Lindex" v-if="learn != undefined" class="column">
+              <div class="column is-three-quarters">
+              </div>
+              <div class="nope" v-if="learn != 0 && learn.isSkill">
+                {{Lindex+1}}
+              </div>
+              <div class="yes" v-if="learn != 0 && !learn.isSkill">
+                <center>
+                  {{Lindex+1}}
+                </center>
+              </div>
+              <div class="nope" v-if="learn == 0">
+                {{Lindex+1}}
+              </div>
+            </td>
+          </div>
+        </tr>
+      </table>
+
+      <br>
+      <h5 class="title is-5">
+        <center>
+          <table>
+            <tr v-for="(talentEachLvl,index) in sortedTalent" :key="talentEachLvl[0]">
+              <td :class="guide.talentTree[index] == 0 ? 'use' : ''">{{talentEachLvl[0]}}</td>
+              <td>Level{{25-(5*index)}}</td>
+              <td :class="guide.talentTree[index] == 1 ? 'use' : ''">{{talentEachLvl[1]}}</td>
+              <br>
+            </tr>
+          </table>
+        </center>
+      </h5>
       <br>
     </div>
-
-    <table>
-      <tr v-for="(skill,Sindex) in hero.skills" :key="skill.name">
-        <div>{{skill.name}}</div><br>
-        <td v-for="(learn,Lindex) in guide.learnOrder" :key="Lindex" v-if="learn != undefined">
-          <div v-if="learn != 0 && Sindex == learn.slot">
-            1
-          </div>
-          <div v-if="learn != 0 && Sindex != learn.slot">
-            0
-          </div>
-          <div v-if="learn == 0">
-            0
-          </div>
-        </td>
-      </tr>
-      <tr>
-        talents
-        <td v-for="(learn,Lindex) in guide.learnOrder" :key="Lindex" v-if="learn != undefined">
-          <div v-if="learn != 0 && learn.isSkill">
-            0
-          </div>
-          <div v-if="learn != 0 && !learn.isSkill">
-            1
-          </div>
-          <div v-if="learn == 0">
-            0
-          </div>
-        </td>
-      </tr>
-    </table>
-
-    <br>
-
-    <h2>Talents</h2>
-    <table>
-      <tr v-for="(talentEachLvl,index) in sortedTalent" :key="talentEachLvl[0]">
-        <td>{{talentEachLvl[0]}}</td>
-        <td>Level{{25-(5*index)}}</td>
-        <td>{{talentEachLvl[1]}}</td>
-
-      </tr>
-    </table>
-
-    <div v-for="talent in guide.talentTree" :key="talent">
-      {{talent}}
-    </div>
-    <br>
-  </div>
 </template>
 
 <script>
@@ -104,5 +162,15 @@ export default {
 </script>
 
 <style scoped>
-
+.nope {
+  background-color: black;
+  color: black;
+}
+.yes {
+  background-color: red;
+  color: white;
+}
+.use {
+  background-color: gold;
+}
 </style>
