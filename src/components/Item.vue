c80548fd830af8027eac6236a0ce26a697fadb40
Withee Poositasai <witheep@gmail.com>
2017-12-11 15:32:00 +0700

    Update item UI

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,8 +1,8 @@
 <template>
-  <div>
+  <div v-if="itemKey && items[itemKey]">
     <div class="columns">
       <div class="column">
-        <h1 class="title is-1">{{item.name}}</h1>
+        <h1 class="title is-1">{{items[this.itemKey].name}}</h1>
       </div>
       <div class="column is-narrow" v-if="$store.state.user && $store.state.user.admin">
         <router-link :to="'/updateitem/'+itemKey">
@@ -13,30 +13,35 @@
 
     <div class="columns">
       <div class="column is-6">
-        <img :src="item.icon" :alt="item.name">
+        <img :src="items[this.itemKey].icon" :alt="items[this.itemKey].name">
         <h5 class="title is-5">Lore</h5>
-        <p>{{item.lore}}</p>
+        <p>{{items[this.itemKey].lore}}</p>
         <br>
-        <p>Category: {{item.category}}</p>
+        <p>Category: {{items[this.itemKey].category}}</p>
         <br>
-        <p>Price : {{item.price}}</p>
 
-        <p>Recipes:
-          <span v-for="recipe in item.recipe" :key="recipe">{{recipe}} </span>
-        </p>
-        <p>Recipe Price: {{item.recipePrice}}</p>
+        <p>Total Price : {{items[this.itemKey].price}}</p>
+
+        <div v-if="items[this.itemKey].recipe">
+          <p>Recipes: </p>
+          <ul>
+            <li v-for="recipe in items[this.itemKey].recipe" :key="recipe">-
+              <router-link :to="'/item/'+recipe">{{items[recipe].name}}</router-link> ({{items[recipe].price}})</li>
+            <li v-if="items[this.itemKey].recipePrice">- Recipe ({{items[this.itemKey].recipePrice}})</li>
+          </ul>
+        </div>
       </div>
       <div class="column is-2">
         <h5 class="title is-5">Stat</h5>
         <ul>
-          <li v-for="stat in item.stats" :key="stat.name">{{stat.name}}:
+          <li v-for="stat in items[this.itemKey].stats" :key="stat.name">{{stat.name}}:
             <i>{{stat.value}}</i>
           </li>
         </ul>
       </div>
       <div class="column is-4">
         <h5 class="title is-5">Ability</h5>
-        <div v-for="ability in item.abilities" :key="ability.name">
+        <div v-for="ability in items[this.itemKey].abilities" :key="ability.name">
           <h6 class="title is-6">{{ability.name}}</h6>
           <p>Type : {{ability.abilityType}}</p>
           <p>Description : {{ability.description}}</p>
@@ -46,6 +51,7 @@
               <i>{{attr.value}}</i>
             </li>
           </ul>
+          <br>
         </div>
       </div>
 
@@ -61,24 +67,16 @@ var itemDB = Firebase.database().ref('/Items')
 export default {
   name: 'Item',
   props: ['itemKey'],
-  mounted() {
-    // Firebase bind
-    this.$bindAsObject('item', itemDB.child(this.itemKey))
-  },
+
   data() {
-    return {
-      item: {}
+    return {}
+  },
+  firebase: {
+    items: {
+      source: itemDB,
+      asObject: true
     }
   }
-  //   computed: {
-  //     sortedTalent() {
-  //       if(this.hero !== undefined && this.hero.talents !== undefined) {
-  //         return this.hero.talents.reverse()
-  //       } else {
-  //         return null
-  //       }
-  //     }
-  //   },
 }
 </script>
 
